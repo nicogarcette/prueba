7fddc892dd80dcb91dc4d4d3181d10616256e269
nicogarcette <nicolasgarcette@outlook.com>
2023-02-28 11:03:52 -0300

    Prueba para ver que ignora prueba para ver si el ignore, ignora la carpeta Propeties.

---
+++ chauworld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace chauworld {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.Write("buscando el porque se subio la carpeta properties");
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
