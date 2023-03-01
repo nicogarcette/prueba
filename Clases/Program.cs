9a42a5186e6de3e305d4b1b2197425ac25c85dab
nicogarcette <nicolasgarcette@outlook.com>
2023-03-01 11:44:54 -0300

    Clase abstracta.

---
+++ Clases/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("hola");
+
+            Console.ReadKey();
+
+
+        }
+    }
+
+    public abstract class Triangulo{
+        
+        abstract public decimal Perimetro();// el metodo abstracto va sin las llaves
+    }
+    public class escaleno:Triangulo {
+
+        public override decimal Perimetro() {
+
+            return 10;
+        
+        }
+    }
+}
