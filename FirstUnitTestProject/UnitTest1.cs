399c129e02a903bd30d7d129b40d90d4a3a565a7
nicogarcette <nicolasgarcette@outlook.com>
2023-03-02 15:31:07 -0300

    Con uni Test se realizo un testing de la funcion EsPrimo

---
+++ FirstUnitTestProject/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace FirstUnitTestProject {
+    [TestClass]
+    public class UnitTest1 {
+        [TestMethod]
+        public void TestMethod1() {
+            // the reference to the project must be added
+
+            bool result = Testing.Program.EsPrimo(23);
+
+            Assert.AreEqual(true,result);
+        }
+    }
+}
---
+++ Testing/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing {
+    public class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("---UniTest---");
+
+
+
+            Console.WriteLine(EsPrimo(5));
+
+            Console.ReadKey();
+        }
+
+        public static bool EsPrimo(int num) {
+            int d = 2;
+            bool es = true;
+
+            while (es && d < num) {
+                if (num%d == 0) {
+                    es = false;
+                }
+
+                d++;
+            }
+
+            return es;
+        }
+    }
+}
