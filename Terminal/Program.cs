cb71a187b332d61dff48e4bf0e40a56775914c55
nicogarcette <nicolasgarcette@outlook.com>
2023-03-02 11:53:07 -0300

    Se realizo la practica1
    se creo las clases taxi y Omnibus, las cuales heredan de la clase abstracta TransportePublico

--- Terminal/Program.cs
+++ Terminal/Program.cs
@@ -9,6 +9,20 @@ namespace Terminal {
         static void Main(string[] args) {
             Console.WriteLine("\n------Terminal------\n");
 
+            List<TransportePublico> transportePublicos = new List<TransportePublico>();
+
+            for (int i = 0; i < 5; i++) {
+                transportePublicos.Add(new Taxi(i + 1));
+                transportePublicos.Add(new Omnibus((i + 2)*10));
+            }
+
+            foreach (var transporte in transportePublicos) {
+                transporte.Avanzar();
+            }
+            Console.WriteLine("\n....accidente en mitre y josePerez\n");
+            foreach (var transporte in transportePublicos) {
+                transporte.Detenerse();
+            }
 
             Console.ReadKey();
         }
---
+++ Terminal/TransportePublico.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Terminal {
+    public abstract class TransportePublico {
+
+        //public int pasajeros { get; set; }
+        protected int pasajeros;
+
+        public TransportePublico(int pasajeros) {
+
+            this.pasajeros = pasajeros;
+        }
+
+        public abstract void Avanzar();
+        public abstract void Detenerse();
+    }
+    public class Omnibus : TransportePublico {
+
+        public Omnibus(int pasajeros): base(pasajeros) {
+
+        }
+
+        public override void Avanzar() {
+
+            Console.WriteLine($"Arranca el bondi con {pasajeros} pasajeros");
+        }
+        public override void Detenerse() { 
+        
+            Console.WriteLine($"frena el bondi con {pasajeros} pasajeros");
+        }
+    }
+    public class Taxi : TransportePublico {
+
+        public Taxi(int pasajeros) : base(pasajeros) {
+
+        }
+
+        public override void Avanzar() {
+
+            Console.WriteLine($"Arranca el taxi con {pasajeros} pasajeros");
+        }
+        public override void Detenerse() {
+
+            Console.WriteLine($"frena el taxi con {pasajeros} pasajeros");
+        }
+    }
+
+}
